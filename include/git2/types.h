cd2b37cb6b4950ac4acb2ab3b640eabe2dcaf261
Edward Thomson <ethomson@microsoft.com>
2014-10-03 19:02:29 -0400

    s/git_merge_head/git_annotated_commit
    Rename git_merge_head to git_annotated_commit, as it becomes used
    in more operations than just merge.

--- include/git2/types.h
+++ include/git2/types.h
@@ -171,8 +171,8 @@ typedef struct git_reference git_reference;
 /** Iterator for references */
 typedef struct git_reference_iterator  git_reference_iterator;
 
-/** Merge heads, the input to merge */
-typedef struct git_merge_head git_merge_head;
+/** Annotated commits, the input to merge and rebase. */
+typedef struct git_annotated_commit git_annotated_commit;
 
 /** Merge result */
 typedef struct git_merge_result git_merge_result;
